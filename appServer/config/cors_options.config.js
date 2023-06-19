e287c8094653c25bd32f888a01e4b3e9d0758df2
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-06-19 14:18:48 +0700

    a

--- appServer/config/cors_options.config.js
+++ appServer/config/cors_options.config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: function (origin, callback) {
     // Danh sách các địa chỉ IP được phép
-    const allowedIPs = ['192.168.1.15', '192.168.1.7', '192.168.31.18'];
+    const allowedIPs = ['192.168.1.15', '192.168.1.7', '171.245.160.248'];
 
     // Kiểm tra xem địa chỉ IP của yêu cầu có nằm trong danh sách được phép hay không
     if (allowedIPs.includes(origin)) {
