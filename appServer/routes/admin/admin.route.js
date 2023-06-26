ef6f903f138428a203a404ce78083bbbb8735821
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-06-26 22:11:33 +0700

    new

--- appServer/routes/admin/admin.route.js
+++ appServer/routes/admin/admin.route.js
@@ -3,9 +3,11 @@ const AdminRouter = express.Router();
 const authorize = require('../../middlewares/authorize.middleware');
 const EnumServerDefinitions = require('../../common/enums/enum_server_definitions');
 const FacultyRouter = require('./facutly.route');
+const DepartmentRouter = require('./department.route');
 
 AdminRouter.use(authorize([EnumServerDefinitions.ROLE.ADMIN]));
 AdminRouter.use('/faculties', FacultyRouter);
-AdminRouter
+AdminRouter.use('departments', DepartmentRouter);
+
 
 module.exports = AdminRouter;
