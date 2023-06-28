b770f01c90efc7d1c8668c17aadfb8835d3d3c21
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-06-28 14:33:38 +0700

    new

--- appServer/routes/admin/classroom.admin.route.js
+++ appServer/routes/admin/classroom.admin.route.js
@@ -3,7 +3,7 @@ const ClassroomAdminRouter = express.Router();
 const ClassroomController = require('../../controllers/classroom.controller');
 
 ClassroomAdminRouter.get('/', ClassroomController.getAllClassroomsInit);
-ClassroomAdminRouter.get('/get-teacher-subject-regularclass', ClassroomController.getTeacherAndSubjectAndRegularClass);
+ClassroomAdminRouter.get('/get-teachers-subjects-regularclass', ClassroomController.getTeacherAndSubjectAndRegularClass);
 ClassroomAdminRouter.post('/create-classroom', ClassroomController.createClassroom);
 ClassroomAdminRouter.patch('/update-classroom', ClassroomController.updateClassroom);
 ClassroomAdminRouter.delete('/delete-classroom/:classroom_id', ClassroomController.deleteClassroom);
