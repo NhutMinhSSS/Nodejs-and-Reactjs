961b29d0e1de6173ec06568111385b3a35c1b0d9
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-06-28 09:44:33 +0700

    new

--- appServer/routes/admin/subject.admin.route.js
+++ appServer/routes/admin/subject.admin.route.js
@@ -3,9 +3,9 @@ const SubjectAdminRouter = express.Router();
 const SubjectController = require('../../controllers/subject.controller'); 
 
 SubjectAdminRouter.get('/', SubjectController.getAllSubjectsInit);
-// SubjectAdminRouter.get('/get-subjects', SubjectController.getListSubjects);
-// SubjectAdminRouter.post('/create-subject', SubjectController.addSubject);
-// SubjectAdminRouter.patch('/update-subject', SubjectController.updateSubject);
-// SubjectAdminRouter.delete('/delete-subjects/:subject_id', SubjectController.deleteSubject);
+SubjectAdminRouter.get('/get-subjects', SubjectController.getListSubjects);
+SubjectAdminRouter.post('/create-subject', SubjectController.addSubject);
+SubjectAdminRouter.patch('/update-subject', SubjectController.updateSubject);
+SubjectAdminRouter.delete('/delete-subjects/:subject_id', SubjectController.deleteSubject);
 
 module.exports = SubjectAdminRouter;
