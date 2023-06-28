91a17c5f07fbf8b11d21d3d8f632c08991e9005c
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-06-29 00:00:38 +0700

    new

--- appServer/routes/admin/classroom.admin.route.js
+++ appServer/routes/admin/classroom.admin.route.js
@@ -6,6 +6,7 @@ ClassroomAdminRouter.get('/', ClassroomController.getAllClassroomsInit);
 ClassroomAdminRouter.get('/get-teachers-subjects-regularclass', ClassroomController.getTeacherAndSubjectAndRegularClass);
 ClassroomAdminRouter.post('/create-classroom', ClassroomController.createClassroom);
 ClassroomAdminRouter.patch('/update-classroom', ClassroomController.updateClassroom);
+ClassroomAdminRouter.patch('/update-status-classroom', ClassroomController.CloseAndActiveClassroom);
 ClassroomAdminRouter.delete('/delete-classroom/:classroom_id', ClassroomController.deleteClassroom);
 
 module.exports = ClassroomAdminRouter;
