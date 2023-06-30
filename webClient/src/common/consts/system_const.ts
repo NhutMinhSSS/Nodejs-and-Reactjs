4cc7977c257055ecdd77b9c94829dfd73db15db0
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-06-30 20:23:42 +0700

    new API

--- webClient/src/common/consts/system_const.ts
+++ webClient/src/common/consts/system_const.ts
@@ -1,5 +1,5 @@
 class SystemConst {
-    static DOMAIN = 'http://20.39.197.125:3000/api';
+    static DOMAIN = 'https://20.39.197.125:3443/api';
     static STATUS_CODE = {
         SUCCESS: 200,
         BAD_REQUEST: 400,
