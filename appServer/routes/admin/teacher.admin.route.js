7ccddf5745319dd98869bf55e2bd84bbd970bdce
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-07-01 20:02:01 +0700

    new

--- appServer/routes/admin/teacher.admin.route.js
+++ appServer/routes/admin/teacher.admin.route.js
@@ -7,7 +7,7 @@ TeacherAdminRouter.post('/create-teacher', TeacherController.addTeacher);
 TeacherAdminRouter.patch('/update-teacher', TeacherController.updateTeacher);
 TeacherAdminRouter.delete('/delete/:teacher_id', TeacherController.deleteTeacher);
 TeacherAdminRouter.get('/get-teachers-not-in-classroom/:classroom_id', TeacherController.getTeachersListNotInClassroom);
-TeacherAdminRouter.post('add-teachers-to-classroom', TeacherController.addTeachersToClassroom);
+TeacherAdminRouter.post('/add-teachers-to-classroom', TeacherController.addTeachersToClassroom);
 TeacherAdminRouter.put('/remove-teachers-from-classroom', TeacherController.removeTeachersFromClassroom);
 
 module.exports = TeacherAdminRouter;
--- appServer/services/classroom_services/classroom_teacher.service.js
+++ appServer/services/classroom_services/classroom_teacher.service.js
@@ -52,13 +52,47 @@ class ClassroomTeacherService {
     }
     async addTeachersToClassroom(teacherIds, classroomId, transaction) {
         try {
-            const listTeachers = teacherIds.map(item => ({
-                classroom_id: classroomId,
-                teacher_id: item,
-                status: EnumServerDefinitions.STATUS.ACTIVE
-            }));
-            const newListTeachers = await TeacherList.bulkCreate(listTeachers, { updateOnDuplicate: ['status'], transaction });
-            return newListTeachers;
+            // const existingTeacherIds = await TeacherList.findAll({
+            //     where: {
+            //         teacher_id: {[Op.in]: teacherIds},
+            //         classroom_id: classroomId
+            //     },
+            //     attributes: ['teacher_id'],
+            //     transaction
+            // });
+            // const teachersToUpdate = existingTeacherIds.map(({ teacher_id }) => teacher_id);
+            // const teachersToInsert = teacherIds.filter(teacherId => !teachersToUpdate.includes(teacherId));
+            // if (teachersToInsert.length !== EnumServerDefinitions.EMPTY) {
+            //     const teacherListInsert =  teachersToInsert.map(teacherId => ({
+            //         classroom_id: classroomId,
+            //         teacher_id: teacherId,
+            //       }));
+            //     await TeacherList.bulkCreate(teacherListInsert ,
+            //         { transaction, updateOnDuplicate: ['status'] }
+            //       );
+            // }
+            // if (teachersToUpdate.length !== EnumServerDefinitions.EMPTY) {
+            //     await TeacherList.update({ status: EnumServerDefinitions.STATUS.ACTIVE}, {
+            //         where: {
+            //             classroom_id: classroomId,
+            //             teacher_id: {[Op.in]: teacherIds}
+            //         }, transaction
+            //     });
+            // }
+            // return true;
+            const query = `
+            INSERT INTO teacher_lists (classroom_id, teacher_id, status)
+            VALUES (?, ?, ?)
+            ON DUPLICATE KEY UPDATE status = ?
+        `;
+        const values = teacherIds.map(teacherId => [classroomId, teacherId, EnumServerDefinitions.STATUS.ACTIVE, EnumServerDefinitions.STATUS.ACTIVE]);
+
+        await TeacherList.sequelize.query(query, {
+            replacements: values,
+            transaction
+        });
+
+        return true;
         } catch (error) {
             throw error;
         }
