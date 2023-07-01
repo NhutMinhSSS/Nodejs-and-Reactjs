4bbe2dd95fdb5db424e2521b72cd67090df95463
nhutminhsss <nhutminh11102002@gmail.com>
2023-07-01 07:14:18 +0000

    new

--- appServer/routes/admin/classroom.admin.route.js
+++ appServer/routes/admin/classroom.admin.route.js
@@ -4,7 +4,6 @@ const ClassroomController = require('../../controllers/classroom.controller');
 
 ClassroomAdminRouter.get('/', ClassroomController.getAllClassroomsInit);
 ClassroomAdminRouter.get('/get-classroom-detail/:classroom_id', ClassroomController.getListTeachersAndListStudentsByClassroomId);
-ClassroomAdminRouter.get('/get-teacher-student-init/:classroom_id', ClassroomController.getListTeachersByDepartmentIdAndStudents);
 ClassroomAdminRouter.get('/get-teachers-subjects-regularclass', ClassroomController.getTeacherAndSubjectAndRegularClass);
 ClassroomAdminRouter.post('/create-classroom', ClassroomController.createClassroom);
 ClassroomAdminRouter.patch('/update-classroom', ClassroomController.updateClassroom);
