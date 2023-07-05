243127efd4d5411efa8377d20bef879e7dc1df24
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-07-05 11:22:29 +0700

    new

--- appServer/controllers/file.controller.js
+++ appServer/controllers/file.controller.js
@@ -17,9 +17,11 @@ class FileController {
                     EnumMessage.FILE_NOT_EXISTS);
             }
             const filePath = path.join(__dirname, '..', file.file_path, file.physical_name);
-            if (filePath) {
-                return res.download(filePath);
+            if (!filePath) {
+                return ServerResponse.createErrorResponse(res, SystemConst.STATUS_CODE.NOT_FOUND,
+                    EnumMessage.FILE_NOT_EXISTS);
             }
+            return res.download(filePath);
         } catch (error) {
             logger.error(error);
             return ServerResponse.createErrorResponse(res, SystemConst.STATUS_CODE.INTERNAL_SERVER,
