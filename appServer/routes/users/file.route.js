c9481e03d75f2cdac9267c0b6405842e2389b331
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-07-05 10:59:00 +0700

    new

--- appServer/routes/users/file.route.js
+++ appServer/routes/users/file.route.js
@@ -4,6 +4,6 @@ const FileController = require('../../controllers/file.controller');
 const checkPostBelongToClassroom = require('../../middlewares/check_post_classroom.middleware');
 const FileRouter = express.Router();
 
-FileRouter.get('/:file_id', checkPostBelongToClassroom, FileController.sendFileToClient);
+FileRouter.get('/:post_id/:file_id/download', checkPostBelongToClassroom, FileController.sendFileToClient);
 
 module.exports = FileRouter;
