4c422575b349d60fe0a3672e87f6b6936687f8de
nhutminhsss <nhutminh11102002@gmail.com>
2023-07-09 15:01:19 +0000

    new

--- appServer/services/question_services/question.service.js
+++ appServer/services/question_services/question.service.js
@@ -6,11 +6,20 @@ class QuestionService {
     async addQuestionsAndAnswers(questionDataList, postId, transaction) {
         try {
             const DataList = questionDataList.map((item) => {
-                const {answers, ...questionData } = item;
+                const { answers, ...questionData } = item;
+                let extractedAnswers = null;
+              
+                if (answers && answers.length > 0) {
+                  // Trích xuất các trường mong muốn từ answers
+                  extractedAnswers = answers.map(({ answer, correct_answer }) => ({
+                    answer,
+                    correct_answer,
+                  }));
+                }
                 return {
                   exam_id: postId,
                   ...questionData,
-                  ...(questionData.question_category_id !== 3 && answers && answers.length > 0 && { answers }),
+                  ...(questionData.question_category_id !== 3 && extractedAnswers && { answers: extractedAnswers }),
                 };
               });
             // // Tạo danh sách câu hỏi
