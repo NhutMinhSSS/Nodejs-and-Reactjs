3bd5f1978bf49df34141048645f00496d43d4cf7
NhutMinhSSS <nhutminh11102002@gmail.com>
2023-07-11 00:41:01 +0700

    new

--- appServer/controllers/comment.controller.js
+++ appServer/controllers/comment.controller.js
@@ -4,12 +4,16 @@ const SystemConst = require("../common/consts/system_const");
 const EnumMessage = require("../common/enums/enum_message");
 const CommentService = require("../services/comment_services/comment.service");
 const db = require("../config/connect_database.config");
+const EnumServerDefinitions = require("../common/enums/enum_server_definitions");
+const TeacherService = require("../services/teacher_services/teacher.service");
+const StudentService = require("../services/student_services/student.service");
 const sequelize = db.getPool();
 
 class CommentController {
     async createComment(req, res) {
         const post = req.post;
         const accountId = req.user.account_id;
+        const role = req.user.role;
         const content = req.body.content;
         if (!content) {
             return ServerResponse.createErrorResponse(res, SystemConst.STATUS_CODE.BAD_REQUEST,
@@ -18,6 +22,9 @@ class CommentController {
         const transaction = await sequelize.transaction();
         try {
             const newComment = await CommentService.createComment(post.id, content, accountId, transaction);
+            const user = role === await (EnumServerDefinitions.ROLE.TEACHER ?  TeacherService.findTeacherByAccountId(accountId) : StudentService.findStudentByAccountId(accountId));
+            newComment.first_name = user.first_name;
+            newComment.last_name = user.last_name;
             await transaction.commit();
             return ServerResponse.createSuccessResponse(res, SystemConst.STATUS_CODE.SUCCESS, newComment);
         } catch (error) {
